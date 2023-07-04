26b8b305956b8391891253136bfa23a526ef9ea8
William Parker <wrparker@gmail.com>
2023-07-04 12:42:58 +0100

    ADDED Controller function to Controller

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,7 +1,9 @@
+import Controller from "./components/Controller"
+
 function App() {
   return (
       <div className="">
-        <div className="text-2xl">Hello World</div>
+        <Controller />
       </div>
   );
 }
